fa514336195d3f2cea6c1c879c610712b89afee6
Monica Marziano <mmarziano1@gmail.com>
2019-10-22 16:54:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 function reducer() {
   let totalBatteries = batteryBatches.reduce((total, element) => total += element, 0)
-  return totalBatteries
+  
 }
 
 function batteries() {
